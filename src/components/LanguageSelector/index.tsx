1d480eac5ee0b3eb22ded87858760262e58340a1
madliani <madliani@hotmail.com>
2024-12-15 17:38:54 +0400

    refactor: refactor language selector

--- src/components/LanguageSelector/index.tsx
+++ src/components/LanguageSelector/index.tsx
@@ -1,12 +1,25 @@
 import { Select, type SelectProps, useId } from "@fluentui/react-components";
+import { useState } from "react";
 
-export const LanguageSelector = (props: SelectProps) => {
+export const LanguageSelector = () => {
+	const defaultLanguage = "TypeScript";
 	const selectId = useId();
+	const [language, setLanguage] = useState(defaultLanguage);
+
+	const onChange: SelectProps["onChange"] = (_, data) => {
+		setLanguage(data.value);
+	};
 
 	return (
 		<>
 			<label htmlFor={selectId}>Language</label>
-			<Select id={selectId} defaultValue="TypeScript" size="medium" {...props}>
+			<Select
+				id={selectId}
+				onChange={onChange}
+				value={language}
+				defaultValue={defaultLanguage}
+				size="medium"
+			>
 				<option>TypeScript</option>
 				<option>PHP</option>
 			</Select>
