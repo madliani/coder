2650810aad910c646dd89c93a39e6d4ade3f3a93
madliani <madliani@hotmail.com>
2024-12-18 01:16:03 +0400

    feat: add centering to the fluent layout

--- src/layout/FluentLayout/index.tsx
+++ src/layout/FluentLayout/index.tsx
@@ -1,10 +1,18 @@
 import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
 import type { ReactNode } from "react";
+import "./index.css";
 
 type Properties = {
 	children: ReactNode;
 };
 
 export const FluentLayout = ({ children }: Readonly<Properties>) => {
-	return <FluentProvider theme={teamsLightTheme}>{children}</FluentProvider>;
+	return (
+		<FluentProvider
+			theme={teamsLightTheme}
+			className="fluent-layout__fluent-provider"
+		>
+			{children}
+		</FluentProvider>
+	);
 };
